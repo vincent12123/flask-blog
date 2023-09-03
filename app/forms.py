a22f3751188b2fff3259f80820143d33a06c3cd7
sukardi <mrsukardi@gmail.com>
2023-09-04 05:31:21 +0700

    create new create_post

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
@@ -7,3 +7,8 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from app import app, db, login_manager
-from app.models import User
+from app.models import User, Post
 from werkzeug.security import generate_password_hash, check_password_hash
-from app.forms import LoginForm  
+from app.forms import LoginForm, PostForm
 from app.models import User  # Ganti dengan lokasi model User Anda
 
 @login_manager.user_loader
@@ -33,6 +33,17 @@ def login():
     
     return render_template('login.html', title='Login', form=form)
 
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
 
