6eae2cc2e5f2ff0e96b33490c07bb1783878b467
sukardi <mrsukardi@gmail.com>
2023-09-05 00:01:16 +0700

    50%

--- app/routes.py
+++ app/routes.py
@@ -4,21 +4,16 @@ from app import app, db, login_manager
 from app.models import User, Post
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.forms import LoginForm, PostForm
-from app.models import User  # Ganti dengan lokasi model User Anda
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('index.html')
-
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))  # Ganti 'home' dengan nama route halaman utama Anda
+        return redirect(url_for('home'))
     
     form = LoginForm()
     if form.validate_on_submit():
@@ -27,11 +22,22 @@ def login():
         if user and check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))  # Ganti 'home' dengan nama route halaman utama Anda
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/home")
+def home():
+    posts = Post.query.all()  # Ambil semua post dari database
+    return render_template('index.html', posts=posts)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
@@ -45,5 +51,14 @@ def new_post():
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
+@app.route("/posts")
+@login_required
+def posts():
+    all_posts = Post.query.all()  # Mengambil semua posting dari database
+    return render_template('posts.html', posts=all_posts)
 
+@app.route("/post/<int:post_id>")
+def post_detail(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_detail.html', post=post)
 
